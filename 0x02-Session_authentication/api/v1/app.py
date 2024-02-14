d8edca7331d429349118a67244ed3bda428fad01
abdelmalek <abdelmalek@gmail.com>
2024-02-14 15:22:33 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,9 +58,8 @@ def before_request():
                       '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, excluded_paths):
-        if not (
-                auth.authorization_header(request) and
-                auth.session_cookie(request)):
+        if not auth.authorization_header(request) or not
+        auth.session_cookie(request):
             abort(401)
         request.current_user = auth.current_user(request)
         if not request.current_user:
